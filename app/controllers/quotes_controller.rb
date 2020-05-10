fccdec3728c18112a1e8fb8b024e21bc49b6ee01
Sharlin Joseph <sharlinjoseph22@gmail.com>
2020-05-10 00:50:16 +0000

    user input quote page setup

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,21 @@
 class QuotesController < ApplicationController
-def index
-  @quote = Quote.order("RANDOM()").first
-end
+  def index
+    @quote = Quote.order("RANDOM()").first
+  end
 
- def new
+  def new
     @quote = Quote.new
   end
-end
 
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+end
 
